a425da5bb8a3e3c764ca57d9ffcda15a19cf6263
idealistic91 <frerichs.jens@hotmail.de>
2020-02-18 20:28:59 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'start#index'
   get 'start', to: 'start#index'
   post 'fetch', to: 'start#fetch'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
